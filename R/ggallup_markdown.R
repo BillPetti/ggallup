a0f0bd996ab044573b27babe1cec13d813fa3494
Bill Petti <billpetti@gmail.com>
2016-12-20 08:30:49 -0500

    update

--- R/ggallup_markdown.R
+++ R/ggallup_markdown.R
@@ -1,12 +1,12 @@
-#' Apply Gallup-specific formatting to ggplot objects
+#' Apply Gallup-specific formatting to ggplot objects used in markdown documents
 #'
 #' @param plot A ggplot object to apply the formatting to
 #' @export
 #' @examples
 #' \dontrun{ggallup(plot)}
 
-ggallup <- function(plot) {
+ggallup_markdown <- function(plot) {
   plot_gallup <- plot +
-    theme_gallup()
+    theme_gallup_md()
   plot_gallup
 }
